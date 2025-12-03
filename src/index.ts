3bf4250d35ac37915671e50e5c12bec10ffb8f6b
Mustafa BAYRAM <programmer1980@gmail.com>
2025-12-04 00:37:41 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -98,8 +98,8 @@ const app = new Elysia()
   // .listen(process.env.PORT || 6182);
   // .listen(8080);
 
-   .listen({
-  port: 8080,
+.listen({
+  port: process.env.PORT || 6182,
   hostname: "0.0.0.0",
 });
 
